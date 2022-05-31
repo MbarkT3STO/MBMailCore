f6ca66da3bb434eecdc7bc26eb0c72f98608666b
MBARK T3STO <mbarktiesto@outlook.com>
2022-05-31 12:01:53 +0100

    Feat(MailExtensions): Create "SetUsername" Extension method

--- MBMailCore/Extensions/MailExtensions.cs
+++ MBMailCore/Extensions/MailExtensions.cs
@@ -4,5 +4,15 @@ namespace MBMailCore.Extensions;
 
 public static class MailExtensions
 {
+    /// <summary>
+    /// Set the <b>username</b> of the mail sender
+    /// </summary>
+    /// <param name="mail"></param>
+    /// <param name="username">Username</param>
+    public static Mail SetUsername(this Mail mail, string username)
+    {
+        mail.GetType().GetProperty( "Username" )?.SetValue( mail , username );
 
+        return mail;
+    }
 }
