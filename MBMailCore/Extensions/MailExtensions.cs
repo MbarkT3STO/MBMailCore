065cc024351db03f3ac72eaf4b1088afdff5c205
MBARK T3STO <mbarktiesto@outlook.com>
2022-05-31 12:05:57 +0100

    Feat(MailExtensions): Create "SetPassword" Extension method

--- MBMailCore/Extensions/MailExtensions.cs
+++ MBMailCore/Extensions/MailExtensions.cs
@@ -15,4 +15,16 @@ public static class MailExtensions
 
         return mail;
     }
+
+    /// <summary>
+    /// Set the <b>Password</b> of the mail sender
+    /// </summary>
+    /// <param name="mail"></param>
+    /// <param name="password">Password</param>
+    public static Mail SetPassword(this Mail mail, string password)
+    {
+        mail.GetType().GetProperty("Username")?.SetValue(mail, password);
+
+        return mail;
+    }
 }
