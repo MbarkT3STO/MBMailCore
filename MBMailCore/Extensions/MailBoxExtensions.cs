4e8dc8eb2fb0df6920122db25bcb52b2bf5be808
MBARK T3STO <mbarktiesto@outlook.com>
2022-06-06 15:20:53 +0100

    Feat(MailBoxExtensions): Create "Host" Extension method

--- MBMailCore/Extensions/MailBoxExtensions.cs
+++ MBMailCore/Extensions/MailBoxExtensions.cs
@@ -1,6 +1,18 @@
-﻿namespace MBMailCore.Extensions;
+﻿using GemBox.Email.Pop;
+using MBMailCore.Core;
+
+namespace MBMailCore.Extensions;
 
 public static class MailBoxExtensions
 {
-    
+    /// <summary>
+    /// Indicates the email provider's host
+    /// </summary>
+    /// <returns></returns>
+    public static MailBox Host(this MailBox mailBox, string host)
+    {
+        mailBox.PopClient = new PopClient( host );
+
+        return mailBox;
+    }
 }
