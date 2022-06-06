4ecced5bb169202871e3c00559faaad96e78c427
MBARK T3STO <mbarktiesto@outlook.com>
2022-06-06 15:11:27 +0100

    Feat: Create "MailBox" Class

---
+++ MBMailCore/Core/MailBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mail;
+using GemBox.Email.Pop;
+
+namespace MBMailCore.Core;
+
+/// <summary>
+/// Represents an Email Box
+/// </summary>
+public class MailBox
+{
+    #region Properties
+
+    public PopClient PopClient { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public MailBox(string host)
+    {
+        PopClient = new PopClient( host );
+    } 
+    
+    public MailBox(string host, int port)
+    {
+        PopClient = new PopClient( host , port );
+    }
+
+    #endregion
+}
