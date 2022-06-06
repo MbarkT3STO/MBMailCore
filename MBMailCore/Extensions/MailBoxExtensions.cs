bd0a26ee96f3966d7fad4af90b0dcca561d962b8
MBARK T3STO <mbarktiesto@outlook.com>
2022-06-06 15:27:17 +0100

    Feat(MailBoxExtensions): Create an overload of "Host" Extension method that set the [Host] and [Port]

--- MBMailCore/Extensions/MailBoxExtensions.cs
+++ MBMailCore/Extensions/MailBoxExtensions.cs
@@ -8,10 +8,24 @@ public static class MailBoxExtensions
     /// <summary>
     /// Indicates the email provider's host
     /// </summary>
-    /// <returns></returns>
+    /// <param name="mailBox"></param>
+    /// <param name="host">Host</param>
     public static MailBox Host(this MailBox mailBox, string host)
     {
         mailBox.PopClient = new PopClient( host );
+
+        return mailBox;
+    }
+
+    /// <summary>
+    /// Indicates the email provider's host and port
+    /// </summary>
+    /// <param name="mailBox"></param>
+    /// <param name="host">Host</param>
+    /// <param name="port">Port</param>
+    public static MailBox Host(this MailBox mailBox, string host, int port)
+    {
+        mailBox.PopClient = new PopClient( host , port );
 
         return mailBox;
     }
